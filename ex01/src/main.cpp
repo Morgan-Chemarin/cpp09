780addb19a9bdad5716d4e5ce08535349121b251
Morgan Chemarin <mchemari@z3r3p2.42lyon.fr>
2026-06-03 10:32:54 +0200

    fix

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mchemari <mchemari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 16:08:45 by mchemari          #+#    #+#             */
-/*   Updated: 2026/05/30 17:25:50 by mchemari         ###   ########.fr       */
+/*   Updated: 2026/06/03 10:31:45 by mchemari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cout << "Too many arguments : ./RPN \"8 9 *\"" << std::endl;
+		std::cout << "Error : bad format ./RPN \"8 9 *\"" << std::endl;
 		return 1;
 	}
 	RPN rpn(av[1]);
