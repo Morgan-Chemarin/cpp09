ddea56adeef29539f6c072f5cef2bc6daf483ff8
Morgan Chemarin <mchemari@z3r3p2.42lyon.fr>
2026-06-03 10:29:18 +0200

    fix: finish RPN

--- ex01/src/RPN.cpp
+++ ex01/src/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: mchemari <mchemari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 16:08:47 by mchemari          #+#    #+#             */
-/*   Updated: 2026/05/30 18:28:12 by mchemari         ###   ########.fr       */
+/*   Updated: 2026/06/01 17:43:03 by mchemari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,32 +49,38 @@ static bool isSymb(char c)
 
 void RPN::calculate()
 {
-	int result = 0;
 	for (size_t i = 0; i < _input.size(); i++)
 	{
 		if (_input[i] == ' ')
 			i++;
 		if (isdigit(_input[i]))
 			_stack.push(_input[i] - '0');
+
+
 		else if (isSymb(_input[i]))
 		{
 			if (_stack.size() < 2)
 			{
-				std::cout << "Error: too many operators" << std::endl;
-				return;
-			}
-			else if (_stack.size() > 2)
-			{
-				std::cout << "Error: not enough operators" << std::endl;
+				std::cout << "Error: not enough numbers" << std::endl;
 				return;
 			}
 
-			if (!result)
-				result = _stack.top();
-			_stack.pop(); // 1 element after
-			result  = result * _stack.top();
-			_stack.pop(); // 0 element after
-			_stack.push(result); // 1 element after
+			int a = _stack.top();
+			_stack.pop();
+			int b = _stack.top();
+			_stack.pop();
+			int result = 0;
+			
+			if (_input[i] == '*')
+				result = b * a;
+			else if (_input[i] == '/')
+				result = b / a;
+			else if (_input[i] == '+')
+				result = b + a;
+			else if (_input[i] == '-')
+				result = b - a;
+
+			_stack.push(result);
 		}
 		else
 		{
@@ -85,5 +91,5 @@ void RPN::calculate()
 	if (_stack.size() == 1)
         std::cout << _stack.top() << std::endl;
 	else
-		std::cout << result << std::endl;
+		std::cout << "Error: invalid expression" << std::endl;
 }
